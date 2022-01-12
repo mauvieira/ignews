e14dda82a513c3dde1db57f7b1597789b108ba9b
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-12 17:31:14 -0300

    :lipstick: finished header style
    finished header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
+import styles from './styles.module.scss';
+
 export function Header() {
   return (
-    <header>
-      <div>
-        <img src="/images/logo.svg" alt="" />
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a href="#" className={styles.active}>Home</a>
+          <a href="#">Posts</a>
+        </nav>
       </div>
     </header>
   )
