f8c37a4cd0511f47af2c256f17fc4a52093bf1e7
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-12 21:54:48 -0300

    :sparkles: added next auth
    added next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.container}>
+  console.log('session', session);
+
+  return session ? (
+    <button type="button" className={styles.container} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Mauricio Vieira
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.container}>
+    <button
+      type="button"
+      className={styles.container}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
