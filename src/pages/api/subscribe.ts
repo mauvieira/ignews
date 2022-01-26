64025660140a33711e0acc548e2d619aedea693d
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-25 21:24:37 -0300

    :sparkles: init subscribe function inside api routes
    init subscribe function inside api routes

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed')
+  }
+}
