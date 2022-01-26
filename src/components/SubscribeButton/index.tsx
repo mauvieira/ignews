5081bbefac2a242a93b85b51162b41f5cd9d0576
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-25 22:07:50 -0300

    :sparkles: finished subscribe function
    finished subscribe function

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
 import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface Props {
@@ -17,7 +18,18 @@ export function SubscribeButton({ priceId }: Props) {
       return;
     }
 
-    await api.post('/subscribe');
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId })
+
+    } catch (error) {
+      alert(error.message)
+    }
 
   }
 
