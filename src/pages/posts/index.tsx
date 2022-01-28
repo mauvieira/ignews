15891b9cb4274d1170d6c35c5638bc90658f8d2f
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-27 21:28:18 -0300

    :lipstick: finished posts page style
    finished posts page style

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -8,19 +8,19 @@ export default function Posts() {
         <title>Posts | Ignews</title>
       </Head>
 
-      <main>
-        <div className={styles.container}>
-          <a>
+      <main className={styles.container}>
+        <div className={styles.content}>
+          <a href='#'>
             <time>21 de janeiro de 2022</time>
             <h2>Top 6 React State Management Libraries for 2022</h2>
             <p>State management is one of the most important aspects of every app. The app’s state dictates what users see, how the app looks, what data is stored, and so on. Thus it’s no wonder that there are so many open-source libraries designed specifically to make state management easier and more enjoyable.</p>
           </a>
-          <a>
+          <a href='#'>
             <time>21 de janeiro de 2022</time>
             <h2>Top 6 React State Management Libraries for 2022</h2>
             <p>State management is one of the most important aspects of every app. The app’s state dictates what users see, how the app looks, what data is stored, and so on. Thus it’s no wonder that there are so many open-source libraries designed specifically to make state management easier and more enjoyable.</p>
           </a>
-          <a>
+          <a href='#'>
             <time>21 de janeiro de 2022</time>
             <h2>Top 6 React State Management Libraries for 2022</h2>
             <p>State management is one of the most important aspects of every app. The app’s state dictates what users see, how the app looks, what data is stored, and so on. Thus it’s no wonder that there are so many open-source libraries designed specifically to make state management easier and more enjoyable.</p>
