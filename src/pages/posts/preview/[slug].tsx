f1f82dbe508bb457d363a0f2f149921791741c76
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-29 15:16:14 -0300

    :sparkles: added revalidate minutes
    added revalidate minutes

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -77,6 +77,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }
 
   return {
-    props: { post }
+    props: {
+      post
+    },
+    revalidate: 60 * 30, // 30 minutes
   }
 }
