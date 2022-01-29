8157e940273a69735878494fddf29f9633034ac4
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-28 22:14:00 -0300

    :lipstick: post page styles
    post page styles

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,9 +1,10 @@
+import Head from 'next/head';
 import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/react"
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 import { formatDate } from '../../util/formatDate';
-
+import styles from './post.module.scss';
 
 interface Props {
   post: {
@@ -22,9 +23,17 @@ export default function Post(props: Props) {
 
   return (
     <>
-      <h1>{title}</h1>
-      <span>{updatedAt}</span>
-      <>{content}</>
+      <Head>
+        <title>{title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{title}</h1>
+          <span>{updatedAt}</span>
+          <div className={styles.content} dangerouslySetInnerHTML={{ __html: content }} />
+        </article>
+      </main>
     </>
   )
 };
